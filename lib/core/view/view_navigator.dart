b4d63e703514310ebe9766f3dffa03ca46550fec
The Mighty Udin <email>
2022-10-11 16:39:34 +0700

    doen

--- lib/core/view/view_navigator.dart
+++ lib/core/view/view_navigator.dart
@@ -1,15 +1,15 @@
 part of '../../android_core.dart';
 
 abstract class ViewNavigator extends StatelessWidget {
-  ViewNavigator({required this.navigatorKey});
   GlobalKey<NavigatorState> navigatorKey;
+  String initialRoute;
+  List<ViewRoute> routes;
+
+  ViewNavigator({required this.navigatorKey, this.routes = const [], this.initialRoute = '/'});
 
   // current arguments and settings
-  Map arguments = {};
-  late RouteSettings settings;
-
-  String get initialRoute;
-  List<ViewRoute> get routes;
+  Map _arguments = {};
+  Map get arguments => _arguments;
 
   @override
   Widget build(BuildContext context) {
@@ -17,8 +17,7 @@ abstract class ViewNavigator extends StatelessWidget {
       key: navigatorKey,
       initialRoute: initialRoute,
       onGenerateRoute: (routeSettings) {
-        settings = routeSettings;
-        arguments = routeSettings.arguments == null ? {} : routeSettings.arguments as Map;
+        _arguments = routeSettings.arguments == null ? {} : routeSettings.arguments as Map;
         if (routes.any((e) => e.routeName == routeSettings.name)) {
           return routes.firstWhere((element) => element.routeName == routeSettings.name);
         }
