a055deedf1c643a73e1b3d1b95eef03d676e6d47
The Mighty Udin <email>
2022-11-14 14:28:25 +0700

    change route back

--- lib/core/helpers/routing/router.dart
+++ lib/core/helpers/routing/router.dart
@@ -28,16 +28,19 @@ class Router<T extends Enum> {
   void back([Menu<T>? previousMenu]) {
     Menu<T> newMenu = menu;
     bool canBack = false;
+    bool canPop = previousMenu != null ? previousMenu.name == menu.name : menu.name == lastMenu?.name;
 
-    Get.until((route) {
-      newMenu = Menu(menu.name, route: route.settings.name);
+    if (canPop) {
+      Get.until((route) {
+        newMenu = Menu(menu.name, route: route.settings.name);
 
-      bool result = route.settings.name == previousMenu?.route;
-      if (previousMenu == null) result = canBack;
-      canBack = true;
+        bool result = route.settings.name == previousMenu?.route;
+        if (previousMenu == null) result = canBack;
+        canBack = true;
 
-      return result;
-    }, id: previousMenu?.name.index ?? menu.name.index);
+        return result;
+      }, id: previousMenu?.name.index ?? menu.name.index);
+    }
 
     debugBack(menu, newMenu);
     lastMenu = null;
