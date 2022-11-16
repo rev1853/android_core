31d21e84cf560a1459932e515a7c96c7ca65c77d
The Mighty Udin <email>
2022-11-16 08:56:42 +0700

    change controllable widget

--- lib/core/view/controllable_view.dart
+++ lib/core/view/controllable_view.dart
@@ -1,5 +1,10 @@
 part of '../../android_core.dart';
 
 abstract class ControllableView<T extends WidgetController> extends StatelessWidget {
+  T controller;
+
+  ControllableView({required this.controller});
+
+  @override
   Widget build(BuildContext context);
 }
