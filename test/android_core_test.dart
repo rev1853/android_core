8852604c18e316ff76dc8bdf6044d2e84aa85525
Revann <rghofur053@gmail.com>
2023-03-12 00:57:43 +0700

    done

--- test/android_core_test.dart
+++ test/android_core_test.dart
@@ -1,17 +1,32 @@
-class Person {
-  @override
+typedef dynamic OnCall(List l);
+
+class VarargsFunction {
+  OnCall _onCall;
+
+  VarargsFunction(this._onCall);
+
+  call() => _onCall([]);
+
   noSuchMethod(Invocation invocation) {
-    print(invocation.memberName);
-    print(invocation.isMethod);
-    print(invocation.positionalArguments);
+    final arguments = invocation.positionalArguments;
+    return _onCall(arguments);
   }
 }
 
+class A<T extends Function> {
+  final myMethod = VarargsFunction((arguments) => print(arguments));
+  T Function(VarargsFunction) func;
+
+  A(this.func);
+
+  T get foil => func(myMethod);
+}
+
 main() {
-  try {
-    Person person = Person();
-    (person as dynamic).call('tes', 'tes');
-  } catch (e) {
-    print(e);
-  }
+  A<Function(String)> a = A(
+    (vovo1) {
+      return (tes) => (vovo1 as dynamic)(tes);
+    },
+  );
+  a.foil('tes');
 }
